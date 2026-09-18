a8427b804b4e21cff993994b7088d1adb9223144
Miyou00 <psychepsycho851@gmail.com>
2026-09-18 18:55:22 +0800

    Update ScrapperLoader.lua

--- ScrapperLoader.lua
+++ ScrapperLoader.lua
@@ -31,7 +31,7 @@
 ------------------------------------------------------------
 
 local WORKER_URL = "https://private-inspector.psychepsycho851.workers.dev"
-local SCRIPT_VERSION = "2.7.0"
+local SCRIPT_VERSION = "2.7.1"
 local MAX_SOURCE_BYTES = 2 * 1024 * 1024
 
 ------------------------------------------------------------
